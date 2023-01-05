9f22c3bb824aa19e519d5bc1bf853fff21ab7400
tigricina <karina6891@mail.ru>
2023-01-05 20:27:55 +0300

    hide preloader and button if there are less than 1 pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+// Settings
 const apiKey = '7dd9f2bc-fccf-4cc0-9713-b7c209c9efdd';
 const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
-
 
 const options = {
 	method: 'GET',
@@ -10,7 +10,31 @@ const options = {
 	},
 };
 
+// Dom elements
 const filmsWrapper = document.querySelector('.films');
+const loader = document.querySelector('.loader-wrapper');
+const btnLoadMore = document.querySelector('.show-more');
+
+// Получение и вывод TOP 250 фильмов
+async function fetchAndRenderFilms() {
+	// Show preloader
+	loader.classList.remove('none')
+
+	// Fetch films data
+	const data = await fetchData(url + 'top', options);
+
+	// checking for additional pages and displaying the button
+	if (data.pagesCount > 1) {
+		// Show the button
+		btnLoadMore.classList.remove('none')
+	}
+
+	// Hide preloader
+	loader.classList.add('none')
+
+	// Render films on page
+	renderFilms(data.films);
+}
 
 async function fetchData(url, options) {
 	const response = await fetch(url, options);
@@ -19,19 +43,9 @@ async function fetchData(url, options) {
 	return data;
 }
 
-async function fetchAndRenderFilms() {
-	//const response = await fetch(url + 'top', options);
-	//const data = await response.json();
-
-	const data = await fetchData(url + 'top', options);
-	
-	renderFilms(data.films);
-}
-
 function renderFilms(films) {
 	for (film of films) {
 		
-
 		const html = `
 					<div class="card">
 					<img src=${film.posterUrlPreview} alt="Cover" class="card__img" />
