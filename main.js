023cc3daf5bca341a4ae23e11fdda2cef0aa477e
tigricina <karina6891@mail.ru>
2023-01-05 17:57:14 +0300

    created 2 functions for getting data and for renders films

--- main.js
+++ main.js
@@ -10,25 +10,35 @@ const options = {
 	},
 };
 
-const filmsWrapper = document.querySelector('.films')
-//console.log(filmsWrapper);
+const filmsWrapper = document.querySelector('.films');
+
+async function fetchData(url, options) {
+	const response = await fetch(url, options);
+	const data = await response.json();
+
+	return data;
+}
 
 async function fetchAndRenderFilms() {
-	const response = await fetch(url + 'top', options);
-	const data = await response.json()
-	console.log(data)
-	console.log(data.films)
+	//const response = await fetch(url + 'top', options);
+	//const data = await response.json();
 
-	for (film of data.films) {
-		console.log(film);
+	const data = await fetchData(url + 'top', options);
+	
+	renderFilms(data.films);
+}
+
+function renderFilms(films) {
+	for (film of films) {
+		
 
 		const html = `
-		<div class="card">
-		<img src=${film.posterUrlPreview} alt="Cover" class="card__img" />
-		<h3 class="card__title">${film.nameRu}</h3>
-		<p class="card__year">${film.year}</p>
-		<p class="card__rate">Рейтинг: ${film.rating}</p
-		></div>`;
+					<div class="card">
+					<img src=${film.posterUrlPreview} alt="Cover" class="card__img" />
+					<h3 class="card__title">${film.nameRu}</h3>
+					<p class="card__year">${film.year}</p>
+					<p class="card__rate">Рейтинг: ${film.rating}</p
+					></div>`;
 									
 		filmsWrapper.insertAdjacentHTML('beforeend', html);
 	}
