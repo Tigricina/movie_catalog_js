9bd1b4759b95ed8f9c3dc6dc47996388eff6691a
tigricina <karina6891@mail.ru>
2023-01-05 15:56:57 +0300

    added Method insertAdjacentHTML for insert a line of HTML code anywhere on the page

--- main.js
+++ main.js
@@ -10,15 +10,22 @@ const options = {
 	},
 };
 
+const filmsWrapper = document.querySelector('.films')
+//console.log(filmsWrapper);
+
 async function fetchAndRenderFilms() {
-	try {
-		const response = await fetch(url + 'top', options);
-		const data = await response.json()
-		console.log(data)
-	} catch(err) {
-		console.log(err)
+	const response = await fetch(url + 'top', options);
+	const data = await response.json()
+	console.log(data)
+	console.log(data.films)
+
+	for (film of data.films) {
+		console.log(film);
+
+		const html = '<div class="card"><img src="./img/cover.webp" alt="Cover" class="card__img"><h3 class="card__title">Обливион</h3><p class="card__year">2018</p><p class="card__rate">Rate 9.2</p></div>';
+
+		filmsWrapper.insertAdjacentHTML('beforeend', html);
 	}
-	
 }
 
-fetchAndRenderFilms();
+fetchAndRenderFilms().catch((err) => console.log(err));
