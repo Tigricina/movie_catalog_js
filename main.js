7745636ad23d284ce10869f753f0daf4fc836b23
tigricina <karina6891@mail.ru>
2023-01-05 14:36:57 +0300

    added async function and first variant of catch

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=2', {
-    method: 'GET',
-    headers: {
-        'X-API-KEY': '7dd9f2bc-fccf-4cc0-9713-b7c209c9efdd',
-        'Content-Type': 'application/json',
-    },
-})
+const apiKey = '7dd9f2bc-fccf-4cc0-9713-b7c209c9efdd';
+const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-API-KEY': apiKey,
+		'Content-Type': 'application/json',
+	},
+};
+
+/*
+fetch(url + 'top', options)
     .then(res => res.json())
     .then(json => console.log(json))
     .catch(err => console.log(err))
+	*/
+
+async function fetchAndRenderFilms() {
+	const response = await fetch(url + 'top', options);
+	const data = await response.json()
+	console.log(data)
+}
+
+fetchAndRenderFilms().catch((err) => console.log(err));
